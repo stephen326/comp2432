b95675fa571188e6f0704c94ecfa54d31c1b1aae
stephen326 <xwq88866@gmail.com>
2024-04-07 22:54:12 +0800

    1.Merged main.c and interface.c together so that they can be executed in a single C file. 2.Implemented checking for exceeding the deadline (DDL) and only assigning orders to SUOs that do not exceed the deadline.

--- interface.c
+++ interface.c
@@ -5,6 +5,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <unistd.h>
+
 
 // 定义订单结构体
 typedef struct {
@@ -13,6 +15,67 @@ typedef struct {
     int quantity[20]; // 数量以字符串形式存储
     char productName[20];
 } Order;
+
+
+#define NUMBER_OF_CHILD 3
+#define READY "R" // student can send this signal to parent to state that he finish on receive 
+#define END "E" // END game signal
+
+
+void executeMainLogic() {
+    int   childPids[NUMBER_OF_CHILD];
+    int   p2cPipe[NUMBER_OF_CHILD][2]; // pipe sent from parent to child
+    int   c2pPipe[NUMBER_OF_CHILD][2]; // pipe sent from child to parent
+    int   n, cNum, endGame = 0;
+    char  buf[80];
+
+
+    for (cNum = 0; cNum < NUMBER_OF_CHILD; cNum++) { // create pipe and child processes
+        if (pipe(p2cPipe[cNum]) < 0) {
+            printf("P2C Pipe creation error\n");
+            exit(1);
+        }
+        if (pipe(c2pPipe[cNum]) < 0) {
+            printf("C2P Pipe creation error\n");
+            exit(1);
+        }
+        if ((childPids[cNum] = fork()) < 0) {
+            printf("Fork failed\n");
+            exit(1);
+        } else if (childPids[cNum] == 0) { // child
+            printf("Child process %d with PID: %d\n", cNum+1, getpid());
+            close(p2cPipe[cNum][1]); // close child out (This pipe is only read from parent)
+            close(c2pPipe[cNum][0]); // close child in (This pipe is only write to parent)
+            if ((n = read(p2cPipe[cNum][0], buf, 80)) > 0) { // read from pipe
+                printf("Child %d received message from parent: %s\n", cNum+1, buf);
+                //开始读取文件了，进行后面的两个步骤，Output Format和Analysis
+            }
+
+            write(c2pPipe[cNum][1], END, sizeof(END)); //子告诉父后面俩步骤完成了
+
+            close(p2cPipe[cNum][0]);
+            close(c2pPipe[cNum][1]);
+            exit(0);
+        } else { // parent
+            printf("Parent created child process %d with PID: %d\n", cNum+1, childPids[cNum]);
+            close(p2cPipe[cNum][0]); // close parent in (This pipe is only read from child)
+            close(c2pPipe[cNum][1]); // close parent out (This pipe is only receive from child)
+            
+            write(p2cPipe[cNum][1], READY, sizeof(READY)); // 父亲给孩子说你可以开始读取文件了，进行后面的两个步骤，Output Format和Analysis
+            
+            if ((n = read(c2pPipe[cNum][0], buf, 80)) > 0) { // 子给父说后面2步骤完成了过后父亲有个反应
+                printf("Parent received message from child %d: %s\n", cNum+1, buf);
+                //随便有个反应就行，目的是为了满足有fork有pipe
+            }
+            close(p2cPipe[cNum][1]); // close parent out
+            close(c2pPipe[cNum][0]); // close parent in
+        
+
+        }
+    }
+}
+
+
 
 
 void addPeriod(char startDate[], char endDate[]) {
@@ -57,65 +120,59 @@ int checkDuplicate(char fileName[], char orderNumber[]) {
 void addOrder(char orderNumber[], char dueDate[], int quantity, char productName[]) {
     FILE *file;
     char fileName[20];
+    char endDate[11]; // 保存期望的截止日期
+
+    // 打开 period.txt 文件以读取期望的截止日期
+    file = fopen("periods.txt", "r");
+    if (file == NULL) {
+        printf("Error opening periods.txt\n");
+        return;
+    }
+
+    // 读取第二个日期
+    if (fscanf(file, "%*s %s", endDate) != 1) {
+        printf("Error reading end date from periods.txt\n");
+        fclose(file);
+        return;
+    }
+    fclose(file);
 
     // 根据产品名称确定文件名
     if (strcmp(productName, "Product_A") == 0 || strcmp(productName, "Product_B") == 0 || strcmp(productName, "Product_C") == 0) {
         strcpy(fileName, "Category_1.txt");
-        
-        // 检查分类文件中是否存在相同的订单号
-        if (checkDuplicate(fileName, orderNumber)) {
-            printf("Order with the same order number already exists in %s\n", fileName);
-            return;
-        }
-        
-        // 打开分类文件以追加模式写入
-        file = fopen(fileName, "a");
-        if (file == NULL) {
-            printf("Error opening file\n");
-            return;
-        }
-        // 将订单信息写入分类文件
-        fprintf(file, "%s %s %d %s\n", orderNumber, dueDate, quantity, productName);
-        fclose(file);
     } 
-    if (strcmp(productName, "Product_D") == 0 || strcmp(productName, "Product_E") == 0 || strcmp(productName, "Product_F") == 0) {
+    else if (strcmp(productName, "Product_D") == 0 || strcmp(productName, "Product_E") == 0 || strcmp(productName, "Product_F") == 0) {
         strcpy(fileName, "Category_2.txt");
-
-        // 检查分类文件中是否存在相同的订单号
-        if (checkDuplicate(fileName, orderNumber)) {
-            printf("Order with the same order number already exists in %s\n", fileName);
-            return;
-        }
-
-        // 打开分类文件以追加模式写入
-        file = fopen(fileName, "a");
-        if (file == NULL) {
-            printf("Error opening file\n");
-            return;
-        }
-        // 将订单信息写入分类文件
-        fprintf(file, "%s %s %d %s\n", orderNumber, dueDate, quantity, productName);
-        fclose(file);
     } 
-    if (strcmp(productName, "Product_G") == 0 || strcmp(productName, "Product_H") == 0 || strcmp(productName, "Product_I") == 0) {
+    else if (strcmp(productName, "Product_G") == 0 || strcmp(productName, "Product_H") == 0 || strcmp(productName, "Product_I") == 0) {
         strcpy(fileName, "Category_3.txt");
-
-        // 检查分类文件中是否存在相同的订单号
-        if (checkDuplicate(fileName, orderNumber)) {
-            printf("Order with the same order number already exists in %s\n", fileName);
-            return;
-        }
-
-        // 打开分类文件以追加模式写入
-        file = fopen(fileName, "a");
-        if (file == NULL) {
-            printf("Error opening file\n");
-            return;
-        }
-        // 将订单信息写入分类文件
-        fprintf(file, "%s %s %d %s\n", orderNumber, dueDate, quantity, productName);
-        fclose(file);
     }
+    else {
+        printf("Invalid product name\n");
+        return;
+    }
+
+    // 检查截止日期是否超出范围
+    if (strcmp(dueDate, endDate) > 0) {
+        printf("Due date exceeds the specified period\n");
+        return;
+    }
+
+    // 检查分类文件中是否存在相同的订单号
+    if (checkDuplicate(fileName, orderNumber)) {
+        printf("Order with the same order number already exists in %s\n", fileName);
+        return;
+    }
+
+    // 打开分类文件以追加模式写入
+    file = fopen(fileName, "a");
+    if (file == NULL) {
+        printf("Error opening file\n");
+        return;
+    }
+    // 将订单信息写入分类文件
+    fprintf(file, "%s %s %d %s\n", orderNumber, dueDate, quantity, productName);
+    fclose(file);
 
     // 打开总订单文件以追加模式写入
     file = fopen("All_Orders.txt", "a");
@@ -127,6 +184,7 @@ void addOrder(char orderNumber[], char dueDate[], int quantity, char productName
     fprintf(file, "%s %s %d %s\n", orderNumber, dueDate, quantity, productName);
     fclose(file);
 }
+
 
 
 // 添加批量订单
@@ -188,13 +246,23 @@ int main() {
         } else if (strcmp(command, "addBATCH") == 0) {
             sscanf(input, "%*s %s", batchFile);
             addBatch(batchFile);
-        } else if (strcmp(command, "runPLS") == 0) {
-            sscanf(input, "%*s %s", algorithm);
-            runPLS(algorithm);
-        } else if (strcmp(command, "printREPORT") == 0) {
-            sscanf(input, "%*s %s", outputFile);
-            printREPORT(outputFile);
-        } else if (strcmp(command, "exitPLS") == 0) {
+        }
+        else  if (strcmp(command, "runPLS") == 0) {
+            sscanf(input, "%*s %s | printREPORT > %s", algorithm, outputFile);
+            // 调用同一文件夹中的main.c中的main函数，并传递算法和输出文件名作为参数
+            executeMainLogic();
+        }
+         
+        
+        // else if (strcmp(command, "runPLS") == 0) {
+        //     sscanf(input, "%*s %s", algorithm);
+        //     runPLS(algorithm);
+        // } else if (strcmp(command, "printREPORT") == 0) {
+        //     sscanf(input, "%*s %s", outputFile);
+        //     printREPORT(outputFile);
+        // }
+
+         else if (strcmp(command, "exitPLS") == 0) {
             printf("Bye-bye!\n");
             exit(0);
         } else {
